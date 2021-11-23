c8edd9ac628aa3bb8bbc3694284207014210b06d
Latrell <lll4xy@virginia.edu>
2021-11-23 14:46:51 -0500

    first 3 sections in part 6B

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'redux'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+
 
 ReactDOM.render(
   <Provider store={store}>
