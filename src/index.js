9c013d357e7ebb0bfc7bd1b8d1e32678c1bbcc90
Latrell <lll4xy@virginia.edu>
2021-11-26 23:36:20 -0500

    part 6-3

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
+import { initializeNotes } from './reducers/noteReducer'
+//import { filterChange } from './reducers/filterReducer'
+import noteService from './services/notes'
 
 const reducer = combineReducers({
   notes: noteReducer,
@@ -18,10 +18,13 @@ const reducer = combineReducers({
 
 const store = createStore(reducer, composeWithDevTools())
 
-console.log(store.getState())
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+noteService.getAll().then(notes =>
+  store.dispatch(initializeNotes(notes))
+)
+// console.log(store.getState())
+// store.subscribe(() => console.log(store.getState()))
+// store.dispatch(filterChange('IMPORTANT'))
+// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 
 ReactDOM.render(
